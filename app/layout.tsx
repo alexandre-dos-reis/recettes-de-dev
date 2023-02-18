20575acb40dd09abeac7d5abaf28e9881aa266dd
Alexandre Dos Reis <alexandre@finishers.com>
2023-02-18 16:47:23 +0100

    Tailwind + layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,9 @@
 import "./globals.css";
 import { getCommandsTree } from "~/mdx/commands/functions";
 import Link from "next/link";
-import { Fragment } from "react";
+import { Fragment, ReactNode } from "react";
 
-interface Props {
-  children: React.ReactNode;
-}
-
-export default async (p: Props) => {
+export default async (p: { children: ReactNode }) => {
   const commands = await getCommandsTree();
 
   return (
@@ -16,35 +12,36 @@ export default async (p: Props) => {
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
+      {/* h-[calc(100vh-2.5rem)] */}
       <head />
-      <body>
-        <header></header>
-        <div>
-          <nav>
-            {commands.map((s) => (
-              <Fragment key={s.id}>
-                <Link style={{ display: "block" }} href={s.slug}>
-                  {s.fm?.title}
-                </Link>
-                {s.children?.length !== 0 ? (
-                  <div style={{ paddingLeft: 20 }}>
-                    {s.children?.map((c) => (
-                      <Link
-                        key={c.id}
-                        style={{ display: "block" }}
-                        href={c.slug}
-                      >
-                        {c.fm?.title}
-                      </Link>
-                    ))}
-                  </div>
-                ) : null}
-              </Fragment>
-            ))}
-          </nav>
-          <main>{p.children}</main>
+      <body className="h-full min-h-full relative">
+        <header className="sticky top-0 bg-blue-400 z-10 h-10">
+          This is the header
+        </header>
+        <div className=" relative min-h-[100vh]">
+          <div className="grid grid-cols-[min-content_auto] relative ">
+            <nav className="bg-red-400 sticky top-10 p-10 h-[calc(100vh-2.5rem)]">
+              {commands.map((s) => (
+                <Fragment key={s.id}>
+                  <Link className="block" href={s.slug}>
+                    {s.fm?.title}
+                  </Link>
+                  {s.children?.length !== 0 ? (
+                    <div>
+                      {s.children?.map((c) => (
+                        <Link className="block ml-5" key={c.id} href={c.slug}>
+                          {c.fm?.title}
+                        </Link>
+                      ))}
+                    </div>
+                  ) : null}
+                </Fragment>
+              ))}
+            </nav>
+            <main className="bg-green-400">{p.children}</main>
+          </div>
         </div>
-        <footer></footer>
+        {/* <footer></footer> */}
       </body>
     </html>
   );
