b474cb9316dd76e1954ab2b6e3213d02779ec2bb
Alexandre Dos Reis <alexandre@finishers.com>
2023-02-18 21:04:06 +0100

    Fixed layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,34 +12,31 @@ export default async (p: { children: ReactNode }) => {
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      {/* h-[calc(100vh-2.5rem)] */}
       <head />
-      <body className="h-full min-h-full relative">
-        <header className="sticky top-0 bg-blue-400 z-10 h-10">
+      <body>
+        <header className="sticky top-0 bg-blue-400 z-10 h-20">
           This is the header
         </header>
-        <div className=" relative min-h-[100vh]">
-          <div className="grid grid-cols-[min-content_auto] relative ">
-            <nav className="bg-red-400 sticky top-10 p-10 h-[calc(100vh-2.5rem)]">
-              {commands.map((s) => (
-                <Fragment key={s.id}>
-                  <Link className="block" href={s.slug}>
-                    {s.fm?.title}
-                  </Link>
-                  {s.children?.length !== 0 ? (
-                    <div>
-                      {s.children?.map((c) => (
-                        <Link className="block ml-5" key={c.id} href={c.slug}>
-                          {c.fm?.title}
-                        </Link>
-                      ))}
-                    </div>
-                  ) : null}
-                </Fragment>
-              ))}
-            </nav>
-            <main className="bg-green-400">{p.children}</main>
-          </div>
+        <div className="grid grid-cols-[min-content_auto]">
+          <nav className="bg-red-400 sticky top-20 p-10 h-[calc(100vh-5rem)]">
+            {commands.map((s) => (
+              <Fragment key={s.id}>
+                <Link className="block" href={s.slug}>
+                  {s.fm?.title}
+                </Link>
+                {s.children?.length === 0 ? null : (
+                  <div>
+                    {s.children?.map((c) => (
+                      <Link className="block ml-5" key={c.id} href={c.slug}>
+                        {c.fm?.title}
+                      </Link>
+                    ))}
+                  </div>
+                )}
+              </Fragment>
+            ))}
+          </nav>
+          <main className="bg-green-400">{p.children}</main>
         </div>
         {/* <footer></footer> */}
       </body>
