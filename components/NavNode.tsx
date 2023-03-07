e5a1c44d17db3d055298fc34abcd0dcc05e647e2
Alexandre Dos Reis <alexandre@finishers.com>
2023-03-07 13:38:03 +0100

    yes finally working

--- components/NavNode.tsx
+++ components/NavNode.tsx
@@ -12,9 +12,9 @@ export const NavNode = ({ docs, pathname, position }: Props) => {
   const lastNode = pathname.split("/").at(-1);
 
   const currentDoc = docs.find((x) => x.node === pathNode);
-  const childrenDocs = currentDoc?.children;
+  const currentDocChildren = currentDoc?.children;
 
-  console.log({ position, currentDoc, childrenDocs});
+  // console.log({ position, currentDoc, currentDocChildren, docs });
 
   if (lastNode === "") {
     return (
@@ -36,7 +36,7 @@ export const NavNode = ({ docs, pathname, position }: Props) => {
 
   return (
     <>
-      {currentDoc ? (
+      {currentDoc && currentDocChildren ? (
         <Link
           href={currentDoc?.slug || ""}
           className={`block whitespace-nowrap ${
@@ -46,10 +46,24 @@ export const NavNode = ({ docs, pathname, position }: Props) => {
           - {currentDoc?.frontmatter.nav ?? currentDoc?.frontmatter.title}
         </Link>
       ) : null}
-
+      <div>
+        {currentDocChildren === undefined
+          ? docs.map((d) => (
+              <Link
+                key={d.id}
+                href={d.slug}
+                className={`block whitespace-nowrap ${
+                  pathname === d.slug ? "underline" : ""
+                }`}
+              >
+                - {d.frontmatter.nav ?? d.frontmatter.title}
+              </Link>
+            ))
+          : null}
+      </div>
       <div className="ml-3">
         {currentDoc?.node === lastNode ? (
-          childrenDocs?.map((d) => (
+          currentDocChildren?.map((d) => (
             <Link
               key={d.id}
               href={d.slug}
@@ -61,11 +75,13 @@ export const NavNode = ({ docs, pathname, position }: Props) => {
             </Link>
           ))
         ) : (
-          <NavNode
-            docs={currentDoc?.children ?? []}
-            pathname={pathname}
-            position={position + 1}
-          />
+          <>
+            <NavNode
+              docs={currentDoc?.children ?? []}
+              pathname={pathname}
+              position={position + 1}
+            />
+          </>
         )}
       </div>
     </>
