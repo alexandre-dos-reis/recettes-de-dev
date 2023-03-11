ae727c0b21475d314993dfd93c51533793cb5ca1
Alexandre Dos Reis <alexandre@finishers.com>
2023-03-11 15:41:50 +0100

    Added tw prose

--- components/CommandItem.tsx
+++ components/CommandItem.tsx
@@ -10,9 +10,9 @@ export const CommandItem = (p: Props) => (
   <section className="mb-10 w-full rounded-lg">
     <div>{p.children}</div>
     <div className="flex items-center gap-3">
-      <code className="bg-slate-800 whitespace-nowrap overflow-x-auto overflow-y-hidden font-mono rounded-lg text-gray-100 p-2 scrollbar-thin scrollbar-thumb-gray-700 scrollbar-track-gray-300">
+      <div className="bg-slate-800 whitespace-nowrap overflow-x-auto overflow-y-hidden font-mono rounded-lg text-gray-100 p-2 scrollbar-thin scrollbar-thumb-gray-700 scrollbar-track-gray-300">
         <span className="text-gray-500">{p.env ?? "$"}</span> {p.i}
-      </code>
+      </div>
     </div>
   </section>
 );
