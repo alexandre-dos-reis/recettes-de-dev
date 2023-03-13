0b2a000195fc1350c141ae02b0088788b75f422f
Alexandre Dos Reis <alexandre@finishers.com>
2023-03-13 06:05:25 +0100

    auto linking headings

---
+++ components/CustomH3.tsx
@@ -0,0 +1,10 @@
+import { DetailedHTMLProps, HTMLAttributes } from "react";
+
+export const CustomH3 = (
+  props: DetailedHTMLProps<
+    HTMLAttributes<HTMLHeadingElement>,
+    HTMLHeadingElement
+  >
+) => {
+  return <h3></h3>;
+};
--- mdx/options.ts
+++ mdx/options.ts
@@ -1,11 +1,11 @@
 import { SerializeOptions } from "next-mdx-remote/dist/types";
 import rehypePrismPlus from "rehype-prism-plus";
 import rehypeSlug from "rehype-slug";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 
 export const options: SerializeOptions = {
   mdxOptions: {
-    rehypePlugins: [rehypePrismPlus, rehypeSlug, rehypeAutolinkHeadings],
+    rehypePlugins: [rehypePrismPlus, rehypeSlug],
   },
   parseFrontmatter: true,
 };
