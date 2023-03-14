f94688fd56c5ca3f81052457a272e68fa9c1adbf
Alexandre Dos Reis <alexandre@finishers.com>
2023-03-14 16:06:18 +0100

    "Folder" Document now closes when selected and clicked

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -8,16 +8,23 @@ interface Props extends Omit<ComponentPropsWithRef<typeof Link>, "href"> {
   pathname?: string;
 }
 
-export const NavLink = ({ doc, pathname, children, ...p }: Props) => (
-  <Link
-    {...p}
-    href={doc?.slug}
-    className={`block whitespace-nowrap border p-1 border-black mb-2 text-center rounded-sm ${
-      pathname === doc.slug ? "underline" : ""
-    }`}
-  >
-    {children}
-    {doc.children ? "> " : ""}
-    {doc.frontmatter.nav ?? doc.frontmatter.title}
-  </Link>
-);
+export const NavLink = ({ doc, pathname, children, ...p }: Props) => {
+  const isSelected = pathname === doc.slug;
+  const hasChildren = doc.children ? true : false;
+  const href =
+    isSelected && hasChildren ? doc.slug.replace(doc.node, "") : doc.slug;
+
+  return (
+    <Link
+      {...p}
+      href={href}
+      className={`block whitespace-nowrap border p-1 border-black mb-2 text-center rounded-sm ${
+        isSelected ? "underline" : ""
+      }`}
+    >
+      {children}
+      {doc.children ? "> " : ""}
+      {doc.frontmatter.nav ?? doc.frontmatter.title}
+    </Link>
+  );
+};
