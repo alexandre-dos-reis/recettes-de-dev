7842b3a5d75dc7207b0f95d34fa237c860ba95cf
Alexandre Dos Reis <alexandre@finishers.com>
2023-03-16 09:50:37 +0100

    patched next-mdx-remote to include headings

--- mdx/compiled-mdx.ts
+++ mdx/compiled-mdx.ts
@@ -7,20 +7,18 @@ import { options } from "./options";
 export const getMdx = async (filepath: string) => {
   const raw = await fs.readFile(filepath, "utf-8");
 
-  const { content, frontmatter } = await compileMDX({
+  return await compileMDX({
     source: raw,
     components: { ...components },
     options,
   } as MDXRemoteProps);
-
-  return { content, frontmatter };
 };
 
 export const getSafeMdx = async <T extends ZodRawShape>(
   filePath: string,
   zodObject: z.ZodObject<T>
 ) => {
-  const { content, frontmatter: unsafeFrontmatter } = await getMdx(filePath);
+  const { frontmatter: unsafeFrontmatter, ...rest } = await getMdx(filePath);
 
   const parsedFrontmatter = zodObject.safeParse(unsafeFrontmatter);
 
@@ -30,5 +28,5 @@ export const getSafeMdx = async <T extends ZodRawShape>(
     );
   }
 
-  return { content, frontmatter: parsedFrontmatter.data };
+  return { frontmatter: parsedFrontmatter.data, ...rest };
 };
