71eeb0a2c12b6135e513a39d5eed44696b356b34
Alexandre Dos Reis <alexandre@finishers.com>
2023-03-27 11:36:01 +0200

    Github repo in env vars

--- utils/env.ts
+++ utils/env.ts
@@ -1,3 +1,4 @@
 export const ENV = {
-  IMAGE_URL: `https://${process.env.NEXT_PUBLIC_IMAGE_URL}/assets` as string,
+  IMAGE_URL: `https://${process.env.NEXT_PUBLIC_IMAGE_URL}/assets`,
+  GITHUB_REPO: `https://github.com/alexandre-dos-reis/recettes-de-dev`,
 };
